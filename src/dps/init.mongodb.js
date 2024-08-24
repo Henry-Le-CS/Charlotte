c957032d5d618ab3602a6e1d5db28ed0a11b4ffa
tiecont <ties.node@outlook.com>
2024-08-24 10:56:02 +0700

    mongo connect

---
+++ src/dps/init.mongodb.js
@@ -0,0 +1,48 @@
+'use strict'
+
+import mongoose from 'mongoose';
+import configENV from '../configs/mongodb.config.js';
+import CheckConnect from '../helpers/check.connect.js';
+
+const { port, host, name} = configENV.db
+// const uri = `mongodb://${host}:${port}/${name}`;
+const uri = process.env.MONGODB_URI
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+// dev
+class database {
+    constructor() {
+        this.connect()
+    }
+
+    connect(type = 'mongodb') {
+        if (1 === 1) {
+            mongoose.set('debug', true)
+            mongoose.set('debug', {color: true})
+        }
+        mongoose.connect(uri, options)
+        .then(() => {
+            console.log(`Connected to database`), CheckConnect.countConnections()
+        })
+        .catch((err) => console.log(err))
+    }
+    
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new database()
+        }
+        return this.instance
+    }
+}
+
+const instanceMongodb = database.getInstance()
+
+export default instanceMongodb
