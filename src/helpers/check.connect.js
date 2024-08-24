f36deab8de4e5f8e934bffab1b4bb72285b59a69
tiecont <ties.node@outlook.com>
2024-08-24 10:56:42 +0700

    updates helpers

--- src/helpers/check.connect.js
+++ src/helpers/check.connect.js
@@ -1,25 +1,26 @@
 'use strict'
-
-import mongoose from "mongoose"
-
-const _SECONDS = 5000
+import { mongoose } from 'mongoose';
+import os from 'os';
+import process from 'process';
+const _SECONDS = 5000;
 export default new class CheckConnection {
-    countConnections() { 
+    countConnections() {
         const numConnections = mongoose.connections.length
-        console.log(`Number of connections: ${numConnections}`)
+        console.log(`Numbers of connections: ${numConnections}`)
     }
-    checkOverLoads() {
+    checkOverLoad() {
         setInterval(() => {
             const numConnections = mongoose.connections.length
             const numCores = os.cpus().length
             const memoryUsage = process.memoryUsage().rss
 
             const maxConnections = numCores * 5
-            console.log(`Activate Connection: ${numConnections}`)
-            console.log(`Memory Usage: ${memoryUsage / 1024 / 1024} MB`)
-
+            console.log(`
+                Active connections: ${numConnections}
+                Memory Usage: ${memoryUsage / 1024 / 1024} MB
+            `)
             if (numConnections > maxConnections) {
-                console.log(`Max Connection reached: ${maxConnections}`)
+                console.log(`Max connections reached: ${maxConnections}`)
                 process.exit(1)
             }
         }, _SECONDS)
