1481d11e1ab280aa1fd290d1cd311934cd73ee88
tiecont <ties.node@outlook.com>
2024-08-24 11:01:10 +0700

    updates router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import check from '../auth/checkAuth.js'
+import userController from '../controllers/user.controller.js'
+import user from './user.router.js'
 const router = express.Router()
 
-router.use(check.apiKey())
-router.use(check.permission())
+router.post('/api/user/register', userController.register)
 
+router.use(check.apiKey)
+router.use(check.permission('admin' || 'create' || 'read' || 'write' || 'delete'))
+
+router.use('/api/user', user)
 export default router
