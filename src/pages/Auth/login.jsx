a449024f762a78b8cb85569fa8d19b3f887c28f8
tiecont <ties.node@outlook.com>
2024-09-06 17:29:28 +0700

    update auth form

---
+++ src/pages/Auth/login.jsx
@@ -0,0 +1,14 @@
+// components
+import AuthLayout from '$/components/AuthLayout/login.jsx';
+import DocumentTitle from '$/components/DocumentTitle';
+
+const Login = () => {
+    return (
+        <>
+            <DocumentTitle title="Login"/>
+            <AuthLayout/>
+        </>
+    )
+}
+
+export default Login
---
+++ src/pages/Auth/signUp.jsx
@@ -0,0 +1,14 @@
+// components
+import SignUp from '$/components/AuthLayout/signUp.jsx';
+import DocumentTitle from '$/components/DocumentTitle';
+
+const Login = () => {
+    return (
+        <>
+            <DocumentTitle title="Register"/>
+            <SignUp/>
+        </>
+    )
+}
+
+export default Login
