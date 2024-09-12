7b40dfef06c7b12980e4c86af3403091fa245895
tiecont <ties.node@outlook.com>
2024-09-12 19:43:45 +0700

    api services

--- src/services/user/index.jsx
+++ src/services/user/index.jsx
@@ -2,6 +2,6 @@
 import userEnpoint from '$/configs/endpoint/user';
 import { axiosInstance } from '$/http/axios-client.jsx';
 const login = formData => axiosInstance({ formData: true }).post(userEnpoint.logining('user'), formData)
-const register = formData => axiosInstance({ formData: true }).post(userEnpoint.register('user'), formData)
+const signup = formData => axiosInstance({ formData: true }).post(userEnpoint.register('user'), formData)
 
-export { login, register };
+export { login, signup };
