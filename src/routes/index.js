2ad2bbad9d27fa46853386eb2479241baee96561
tiecont <ties.node@outlook.com>
2024-09-13 16:26:50 +0700

    add proxy service

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import check from '../auth/checkAuth.js'
 import userController from '../controllers/user.controller.js'
+import proxyService from '../services/proxy.service.js'
 import user from './user.router.js'
 const router = express.Router()
-const permission = 'admin' || 'create' || 'read' || 'write' || 'delete'
-const pmsSelect = ['actions']
 router.post('/api/user/register', userController.register)
 
+router.use('/api', proxyService.createProxy)
 router.use(check.apiKey)
-router.use(check.permission({permission, pmsSelect}))
+router.use(check.permission())
 
 router.use('/api/user', user)
 export default router
