756c02474784d14709708a150e2c4923035f8d87
tiecont <ties.node@outlook.com>
2024-09-16 16:30:39 +0700

    updated app

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS configuration
 var corsOptions = {
-    origin: ['http://localhost:3000', 'https://charlotte-webapp.vercel.app', process.env.VERCEL_SERVER],
+    origin: ['http://localhost:3000', 'https://charlotte-webapp.vercel.app', 'https://charlotte-git-master-undergrsystems-projects.vercel.app'],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: ['Content-Type', 'authorization', 'ngrok-skip-browser-warning', 'x-api-key', 'x-client-id', 'x-rtoken-id'] 
@@ -21,7 +21,7 @@ app.options('*', cors(corsOptions));
 const upload = multer();
 app.use(upload.none());
 app.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:3000', 'https://charlotte-webapp.vercel.app', process.env.VERCEL_SERVER];
+    const allowedOrigins = ['http://localhost:3000', 'https://charlotte-webapp.vercel.app', 'https://charlotte-git-master-undergrsystems-projects.vercel.app'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
