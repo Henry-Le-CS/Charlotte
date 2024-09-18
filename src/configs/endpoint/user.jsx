1698c8dd71c8fa516b8b794779e408b2e92fc4d6
NodeOf Ties <ties.node@outlook.com>
2024-09-19 02:59:41 +0700

    added send email enpoint

--- src/configs/endpoint/user.jsx
+++ src/configs/endpoint/user.jsx
@@ -3,7 +3,7 @@ const userEnpoint = {
     login: (user) => `/${user}/login`,
     // REGISTER
     register: (user) => `/${user}/register`,
-
+    sendEmail: ({ email, userId }) => `/email/send-email?email=${email}&userId=${userId}`,
     // UPDATE INFOMATION
     updateInfomation: (id) => `/user/${id}/updating`,
     search: (value) => `/user/${value}`
