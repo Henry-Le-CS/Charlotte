0f4e167b2f448dd39de300c59373fee465d037b8
tiecont <ties.node@outlook.com>
2024-09-19 13:59:39 +0700

    updated user services

--- src/services/user/index.jsx
+++ src/services/user/index.jsx
@@ -1,9 +1,10 @@
 
+import emailEndpoint from '$/configs/endpoint/email';
 import userEnpoint from '$/configs/endpoint/user';
 import { axiosInstance } from '$/http/axios-client.jsx';
 const login = formData => axiosInstance({ formData: true }).post(userEnpoint.login('user'), formData)
 const signup = formData => axiosInstance({ formData: true }).post(userEnpoint.register('user'), formData)
 const search = value => axiosInstance({ formData: true }).post(userEnpoint.search(value))
-const sendEmail = ({ email, userId }) => axiosInstance({ formData: true }).post(userEnpoint.sendEmail({ email, userId }), { email, userId });
+const sendEmail = ({ email, userId }) => axiosInstance({ formData: true }).get(emailEndpoint.sendEmail({ email, userId }), { timeout: 180000 });
 export { login, search, sendEmail, signup };
 
