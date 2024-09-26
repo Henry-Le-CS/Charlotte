c36feafe74dee3f84817ba60aefc45cdaeceef5b
tiecont <ties.node@outlook.com>
2024-09-26 08:31:34 +0700

    test key

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -62,6 +62,8 @@ export const authentication = asyncHandler( async (req, res, next) => {
 })
 
 export const verifyJWT = (token, keySecret) => {
+    console.log('ACCESS TOKEN:: ', token)
+    console.log('PUBLIC KEY ', keySecret)
     return new Promise((resolve, reject) => {
         JWT.verify(token, keySecret, (err, payload) => {
             if (err) {
