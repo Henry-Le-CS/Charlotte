83f28d676e8d96a5d64cc957a98613b491eea791
tiecont <ties.node@outlook.com>
2024-10-13 23:59:50 +0000

    release key for frontend

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
+import https from 'https';
 import { Server } from 'socket.io';
 import app from './src/app.js';
+import fs from 'fs';
 import userService from './src/services/user.service.js';
 const port = process.env.DEV_APP_PORT
-// Start the server
-const server = app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+
+// SSL KEY
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/cosmetichive.id.vn/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/cosmetichive.id.vn/fullchain.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate, rejectUnauthorized: true, secure : true };
+// Create an HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`Socket.io server is running on port ${port} with HTTPS`);
+}).on('error', (err) => {
+    console.error('Failed to start server:', err);
 });
+
 // io server
-const io = new Server(server);
+const io = new Server(httpsServer, {
+  cors: {
+    origin: ['*'],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
 export const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-    console.log('a user connected to socket')
+    console.log('A user connected to the socket');
     const userId = socket.handshake.query.userId;
-    if (userId != "undefined") userSocketMap[userId] = socket.id;
-    // Listen for custom events
 
-    // io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    // Ensure userId is valid
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    }
 
     // Handle disconnections
     socket.on("disconnect", async () => {
-		await userService.offlineUser(userId)
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+        if (userId) {
+            await userService.offlineUser(userId);
+            delete userSocketMap[userId];
+            io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        }
+    });
 });
+
 // Graceful shutdown on SIGINT (Ctrl+C)
 process.on('SIGINT', () => {
     console.log('SIGINT signal received. Shutting down gracefully...');
-    server.close(() => {
+    httpsServer.close(() => {
         console.log('Server closed');
         process.exit(0); // Exit process after server is closed
     });
@@ -38,10 +62,10 @@ process.on('SIGINT', () => {
 // You can also handle other signals if necessary (e.g., SIGTERM for Kubernetes/PM2):
 process.on('SIGTERM', () => {
     console.log('SIGTERM signal received. Shutting down gracefully...');
-    server.close(() => {
+    httpsServer.close(() => {
         console.log('Server closed');
         process.exit(0);
     });
 });
 
-export default io
+export default io;
